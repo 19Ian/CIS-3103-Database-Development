eebddd78ae7d836b29660bb199530e3c266ec6d9
Ian Hively <ianhively19@gmail.com>
2024-04-18 13:55:33 -0500

    changed plots to methods

--- AthleteDatabaseQueries.py
+++ AthleteDatabaseQueries.py
@@ -43,28 +43,31 @@ except mysql.connector.Error as err:
 else:
     dataCursor = reservationConnection.cursor()
 
+#### SETUP MONTH DATA
+getBirthdateQuery = ("SELECT MONTH(Birthdate) "
+                       "FROM Athlete")
+dataCursor.execute(getBirthdateQuery)
+for row in dataCursor.fetchall():
+    if(row[0] == 1): monthData["January"] += 1
+    if(row[0] == 2): monthData["February"] += 1
+    if(row[0] == 3): monthData["March"] += 1
+    if(row[0] == 4): monthData["April"] += 1
+    if(row[0] == 5): monthData["May"] += 1
+    if(row[0] == 6): monthData["June"] += 1
+    if(row[0] == 7): monthData["July"] += 1
+    if(row[0] == 8): monthData["August"] += 1
+    if(row[0] == 9): monthData["September"] += 1
+    if(row[0] == 10): monthData["October"] += 1
+    if(row[0] == 11): monthData["November"] += 1
+    if(row[0] == 12): monthData["December"] += 1
+
+
+month = list(monthData.keys())
+values = list(monthData.values())
+
 
 #### ATHLETES BORN IN MONTHS BAR CHART ####
-    getBirthdateQuery = ("SELECT MONTH(Birthdate) "
-                       "FROM Athlete")
-    dataCursor.execute(getBirthdateQuery)
-    for row in dataCursor.fetchall():
-        if(row[0] == 1): monthData["January"] += 1
-        if(row[0] == 2): monthData["February"] += 1
-        if(row[0] == 3): monthData["March"] += 1
-        if(row[0] == 4): monthData["April"] += 1
-        if(row[0] == 5): monthData["May"] += 1
-        if(row[0] == 6): monthData["June"] += 1
-        if(row[0] == 7): monthData["July"] += 1
-        if(row[0] == 8): monthData["August"] += 1
-        if(row[0] == 9): monthData["September"] += 1
-        if(row[0] == 10): monthData["October"] += 1
-        if(row[0] == 11): monthData["November"] += 1
-        if(row[0] == 12): monthData["December"] += 1
-
-
-    month = list(monthData.keys())
-    values = list(monthData.values())
+def Born_In_Months():
 
     fig = plt.figure(figsize = (13, 5))
 
@@ -78,6 +81,7 @@ else:
     plt.show()
 
 #### GET BIRTHMONTH, SALARY BAR GRAPH ####
+def Birthmonth_Salary_Bar():
     getBirthdateSalaryQuery = ("SELECT MONTH(Birthdate), AVG(Salary) AS AVG_Salary_Pounds "
                                 "FROM Athlete "
                                 "GROUP BY MONTH(Birthdate) "
@@ -106,5 +110,49 @@ else:
     # plt.savefig('plot3.png')
 
 
+#### GET NAME, WEIGHT, HEIGHT ####
+    # TODO:too many points to plot
+def Name_Weight_Height(firstRow, lastRow):
+    getHeightWeightQuery = ("SELECT FirstName, LastName, Height AS Height_CM, Weight AS Weight_KG "
+                                "FROM Athlete "
+                                "ORDER BY LastName DESC;")
+    dataCursor.execute(getHeightWeightQuery)
 
-    reservationConnection.close()
+    firstNames = []
+    lastNames = []
+    heights = []
+    weights = []
+
+    data = dataCursor.fetchall()
+
+    for i in range(firstRow,lastRow):
+        firstNames.append(data[i][0])
+        lastNames.append(data[i][1])
+        heights.append(data[i][2])
+        weights.append(data[i][3])
+    
+    # for row in dataCursor.fetchall():
+    #     firstNames.append(row[0])
+    #     lastNames.append(row[1])
+    #     heights.append(row[2])
+    #     weights.append(row[3])
+
+    fig = plt.figure(figsize = (14, 10))
+    plt.scatter(heights, weights)
+    plt.xlabel("Height (cm)")
+    plt.ylabel("Weight (kg)")
+    plt.title("Height and Weight")
+
+    for i, txt in enumerate(lastNames):
+        plt.annotate(txt, (heights[i], weights[i]), fontsize=5)
+
+    plt.show()
+
+
+#### PLOTS ####
+# Born_In_Months()
+# Birthmonth_Salary_Bar()
+Name_Weight_Height(50000, 51000)
+
+
+reservationConnection.close()
