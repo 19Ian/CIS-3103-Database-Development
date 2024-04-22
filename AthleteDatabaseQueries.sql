94d6292bccc55f50d0d4074ffca8dc30044fba78
Ian Hively <79172931+19Ian@users.noreply.github.com>
2024-04-22 10:38:44 -0500

    Update AthleteDatabaseQueries.sql

--- AthleteDatabaseQueries.sql
+++ AthleteDatabaseQueries.sql
@@ -1,24 +1,35 @@
-SELECT * FROM athleterecords.athlete;
+SELECT * 
+FROM Athlete
+INNER JOIN AthleteBiometrics ON ID = AthleteID
+INNER JOIN AthleteSalary ON AthleteSalary.AthleteID = AthleteBiometrics.AthleteID;
 
 SELECT FirstName, LastName, Birthdate, Salary AS Salary_Pounds
 FROM Athlete
+INNER JOIN AthleteBiometrics ON ID = AthleteID
+INNER JOIN AthleteSalary ON AthleteSalary.AthleteID = AthleteBiometrics.AthleteID
 ORDER BY Birthdate;
 
 SELECT FirstName, LastName, MONTH(Birthdate), Salary AS Salary_Pounds
-FROM Athlete;
+FROM Athlete
+INNER JOIN AthleteBiometrics ON ID = AthleteID
+INNER JOIN AthleteSalary ON AthleteSalary.AthleteID = AthleteBiometrics.AthleteID;
 
 SELECT MONTH(Birthdate), AVG(Salary) AS AVG_Salary_Pounds
-FROM Athlete
+FROM AthleteBiometrics
+INNER JOIN AthleteSalary ON AthleteSalary.AthleteID = AthleteBiometrics.AthleteID
 GROUP BY MONTH(Birthdate)
 ORDER BY MONTH(Birthdate);
 
-SELECT FirstName, LastName, Height AS Height_CM, Weight AS Weight_KG
+SELECT DISTINCT FirstName, LastName, Height AS Height_CM, Weight AS Weight_KG
 FROM Athlete
-ORDER BY LastName DESC;
+INNER JOIN AthleteBiometrics ON ID = AthleteID;
 
 SELECT AVG(Height) AS Avg_Height_CM, AVG(Weight) AS Avg_Weight_KG
-FROM Athlete;
+FROM AthleteBiometrics;
 
 SELECT FirstName, LastName, Salary AS Salary_Pounds, Nationality, Birthdate
 FROM Athlete
+INNER JOIN AthleteSalary ON ID = AthleteID
+INNER JOIN AthleteBiometrics ON AthleteSalary.AthleteID = AthleteBiometrics.AthleteID
+WHERE Salary > 0
 ORDER BY Salary
